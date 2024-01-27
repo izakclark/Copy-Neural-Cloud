54e464a057652c1fca821c2f35fc732467ce87f4
izakclark <junsanmin51@gmail.com>
2024-01-28 03:59:10 +0900

    24.01.28_in_03 : 54
    Make PlayerHandle func
    
    1. Detect mousceDown and invoke SetObject func and CheckActionAndCuncSelect Coroutine

--- Assets/Scripts/NoneObj/PlayerHandle.cs
+++ Assets/Scripts/NoneObj/PlayerHandle.cs
@@ -6,24 +6,88 @@ using UnityEngine;
 
 public class PlayerHandle : MonoBehaviour
 {
-    
+    public GameObject selectedChar;
+    public GameObject curTile;
+    public GameObject originTile;
+
+    public float distanceFromCam = 10f;
+
+    [Range(0.01f, 1.0f)]
+    public float followSpd = 0.1f;
+
+    private Vector3 mousePos;
+    private Vector3 nextPos;
     private void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButtonDown(0))
         {
-            Vector3 vec3 = Camera.main.WorldToScreenPoint(Input.mousePosition);
-            Debug.Log(vec3);
+            SetObject();
+            StartCoroutine(CheckActionAndFuncSelect());
         }
     }
 
     private void SetObject()
     {
+
         // 터치시 오브젝트와 오브젝트의 위치 확인
+        selectedChar = null;
+        curTile = null;
+        originTile = null;
+
+        RaycastHit[] hits = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+        foreach(RaycastHit hitobj in hits)
+        {
+            if (hitobj.collider.CompareTag("P_CHAR"))
+                selectedChar = hitobj.collider.gameObject;             
+            else if(hitobj.collider.CompareTag("TILE"))
+                originTile = hitobj.collider.gameObject;
+        }
     }
 
-    private void CheckActionAndFuncSelect()
+    private IEnumerator CheckActionAndFuncSelect()
     {
         // 터치 행동에 따라 분기될 기능
+
+        yield return new WaitForSeconds(0.3f);
+
+        bool isGetMouseButton = Input.GetMouseButton(0);
+
+        if(isGetMouseButton)
+        {
+            if (selectedChar != null)
+            {
+                while (isGetMouseButton)
+                {
+                    SetCharPos(Input.GetMouseButton(0));
+
+                    yield return new WaitForSeconds(0.3f);
+
+                    isGetMouseButton = Input.GetMouseButton(0);
+                }
+            }
+            else
+                yield break;
+        }
+        else
+        {
+            ShowInfo();
+            yield break;
+        }
+    }
+
+    private void SetCharPos(bool isGetMouseButton)
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = distanceFromCam;
+
+        nextPos = Camera.main.ScreenToWorldPoint(mousePos);
+        selectedChar.transform.position = Vector3.Lerp(selectedChar.transform.position, nextPos, followSpd);
+    }
+
+    private void ShowInfo()
+    {
+        Debug.Log("4");
     }
 
     private void ZoomInOut()
