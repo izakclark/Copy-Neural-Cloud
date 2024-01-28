68e8ae227a79c9e6cb98cd84a740d36bc5cc1178
izakclark <junsanmin51@gmail.com>
2024-01-29 02:46:23 +0900

    24.01.29 (1)
    타일 배치 알고리즘 개선
    플레이어 핸들 스크립트의 배치 관련 로직 작성

--- Assets/Scripts/NoneObj/PlayerHandle.cs
+++ Assets/Scripts/NoneObj/PlayerHandle.cs
@@ -11,12 +11,13 @@ public class PlayerHandle : MonoBehaviour
     public GameObject originTile;
 
     public float distanceFromCam = 10f;
+    public float howFallbyTile = 5f;
 
     [Range(0.01f, 1.0f)]
     public float followSpd = 0.1f;
 
-    private Vector3 mousePos;
-    private Vector3 nextPos;
+    public float delayTime = 1.0f;
+
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
@@ -28,8 +29,8 @@ public class PlayerHandle : MonoBehaviour
 
     private void SetObject()
     {
-
         // 터치시 오브젝트와 오브젝트의 위치 확인
+
         selectedChar = null;
         curTile = null;
         originTile = null;
@@ -51,19 +52,15 @@ public class PlayerHandle : MonoBehaviour
 
         yield return new WaitForSeconds(0.3f);
 
-        bool isGetMouseButton = Input.GetMouseButton(0);
-
-        if(isGetMouseButton)
+        if(Input.GetMouseButton(0))
         {
             if (selectedChar != null)
             {
-                while (isGetMouseButton)
+                while (Input.GetMouseButton(0))
                 {
-                    SetCharPos(Input.GetMouseButton(0));
-
                     yield return new WaitForSeconds(0.3f);
 
-                    isGetMouseButton = Input.GetMouseButton(0);
+                    PackItUp(Input.GetMouseButton(0));
                 }
             }
             else
@@ -76,13 +73,38 @@ public class PlayerHandle : MonoBehaviour
         }
     }
 
-    private void SetCharPos(bool isGetMouseButton)
+    private void PackItUp(bool isGetMouseButton)
     {
-        mousePos = Input.mousePosition;
-        mousePos.z = distanceFromCam;
+        switch(isGetMouseButton)
+        {
+            case true:
+                RaycastHit[] hits = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition));
 
-        nextPos = Camera.main.ScreenToWorldPoint(mousePos);
-        selectedChar.transform.position = Vector3.Lerp(selectedChar.transform.position, nextPos, followSpd);
+                foreach (RaycastHit hitobj in hits)
+                {
+                    if (hitobj.collider.CompareTag("TILE"))
+                        curTile = hitobj.collider.gameObject;
+                }
+                selectedChar.transform.position =
+                    new Vector3(curTile.transform.position.x, howFallbyTile, curTile.transform.position.z);
+                break;
+                case false:
+                selectedChar.transform.position =
+                    new Vector3(curTile.transform.position.x, howFallbyTile/2, curTile.transform.position.z);
+                break;
+        }
+        
+
+        /*Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float disPosX = Mathf.Abs(pos.x) - Mathf.Abs(selectedChar.transform.position.x);
+        float disPosZ = Mathf.Abs(pos.y) - Mathf.Abs(selectedChar.transform.position.z);
+
+        pos.x = pos.x - disPosX;
+        pos.z = pos.y - disPosZ;
+        pos.y = howFallbyTile;
+
+        selectedChar.transform.position = pos;*/
     }
 
     private void ShowInfo()
--- Assets/Scripts/NoneObj/TileCtrl.cs
+++ Assets/Scripts/NoneObj/TileCtrl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileMakerBehaviour : MonoBehaviour
+public class TileCtrl : MonoBehaviour
 {
     [Tooltip("타일 오브젝트")]
     public GameObject tileObj;
@@ -13,56 +13,57 @@ public class TileMakerBehaviour : MonoBehaviour
 
     public float zScale;
 
-    
     // Start is called before the first frame update
     void Start()
     {
         MakeTile();
+        SetTile();
     }
 
-    // 직선간격 = 오브젝트 스케일 - 0.5
-    // 대각 x간격 = 직선간격 / 2 || 대각 z 간격 = 오브젝트 스케일 - 1
     void MakeTile()
     {
-        float tileDis = tileObj.transform.localScale.x - 0.5f;
-        float x = 0, z = 0;
-        bool isOddNumTile = true;
-
         for(int i = 0; i < xScale * zScale - (xScale - zScale); ++i)
         {
             tiles.Add(Instantiate(tileObj));
             tiles[i].name = i.ToString();
             tiles[i].transform.parent = this.transform;
         }
-        
-        foreach(var tile in tiles)
+       
+    }
+
+    void SetTile()
+    {
+        float tileDis = tileObj.transform.localScale.x - 0.5f; // 4.5
+        float getPosX = tileDis * (int)Mathf.Abs(xScale / 2) * Vector2.left.x; // 13.5
+        float getPosZ = tileDis * (int)Mathf.Abs(zScale / 2) - Vector2.up.y; // 8        
+        float x = 0, z = 0;
+        bool isOddNumTile = true;
+
+        foreach (var tile in tiles)
         {
-            switch(isOddNumTile)
+            switch (isOddNumTile)
             {
                 case false:
-                    tile.transform.position = new Vector3(tileDis / 2 + tileDis* x, 0, (tileDis - 0.5f) * z);
+                    tile.transform.position = new Vector3( getPosX + (tileDis / 2) + tileDis * x, 0, getPosZ - (tileDis - 0.5f) * z);
                     ++x;
-                    if(x == xScale -1)
+                    if (x == xScale - 1)
                     {
                         isOddNumTile = true;
                         x = 0;
-                        --z;
+                        ++z;
                     }
                     break;
                 default:
-                    tile.transform.position = new Vector3(tileDis * x, 0, (tileDis - 0.5f) * z); ;
+                    tile.transform.position = new Vector3(getPosX + tileDis * x, 0, getPosZ - (tileDis -0.5f) * z) ;
                     ++x;
-                    if(x == xScale)
+                    if (x == xScale)
                     {
                         isOddNumTile = false;
                         x = 0;
-                        --z;
+                        ++z;
                     }
                     break;
             }
         }
-        
     }
-
-
 }
